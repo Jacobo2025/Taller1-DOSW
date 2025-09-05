4c0ecb95c20e1e9595711db0414def0bba8c3e49
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2025-09-04 21:42:36 -0500

    Se modificó el README

--- MaquinaMisteriosa.java
+++ MaquinaMisteriosa.java
@@ -2,31 +2,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MaquinaMisteriosa {
-    public void ejecutarComando(String comando){
-        Map<String,Runnable> mensajes = new HashMap<>();
+    Map<String,Runnable> mensajes;
+    public MaquinaMisteriosa() {
+        mensajes = new HashMap<>();
 
         mensajes.put("SALUDAR", () -> System.out.println("¡Saludos, viajero del tiempo y del código!"));
         mensajes.put("DESPEDIR", () -> System.out.println("Que los bits te acompañen, hasta la próxima misión."));
         mensajes.put("CANTAR", () -> System.out.println("01010101"));
         mensajes.put("DANZAR", () -> System.out.println("Girando en modo fiesta."));
+    }
+    public void ejecutarComando(String comando){
 
         switch (comando){
             case "SALUDAR":
-                Runnable mensaje = mensajes.get(comando);
-                mensaje.run();
-                break;
             case "DESPEDIR":
-                Runnable mensaje2 = mensajes.get(comando);
-                mensaje2.run();
-                break;
             case "CANTAR":
-                Runnable mensaje3 = mensajes.get(comando);
-                mensaje3.run();
-                break;
             case "DANZAR":
-                Runnable mensaje4 = mensajes.get(comando);
-                mensaje4.run();
+                mensajes.get(comando).run();
                 break;
+            default:
+                System.out.println("Comando no reconocido.");
+
         }
     }
 }
