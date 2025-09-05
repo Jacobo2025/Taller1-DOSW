a1b074bff9c76776cd926bb4f4413c783ce17c74
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2025-09-04 22:48:55 -0500

    Readme

--- MaquinaMisteriosa.java
+++ MaquinaMisteriosa.java
@@ -4,10 +4,10 @@ public class MaquinaMisteriosa{
     Map<String, Runnable> mensajes;
     public MaquinaMisteriosa(){
         mensajes = new HashMap<>();
-        mensajes.put("BROMEAR", -> System.out.printl("¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio..."));
-        mensajes.put("GRITAR", -> System.out.printl("¡¡¡ALERTA DE STACK OVERFLOW!!!"));
-        mensajes.put("SUSURRAR", -> System.out.printl("Shhh... los bugs están dormidos."));
-        mensajes.put("ANALIZAR", -> System.out.printl("Analizando datos... resultado: ¡Eres increíble programando!"));
+        mensajes.put("BROMEAR",() -> System.out.println("¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio..."));
+        mensajes.put("GRITAR", ()-> System.out.println("¡¡¡ALERTA DE STACK OVERFLOW!!!"));
+        mensajes.put("SUSURRAR",() -> System.out.println("Shhh... los bugs están dormidos."));
+        mensajes.put("ANALIZAR",() -> System.out.println("Analizando datos... resultado: ¡Eres increíble programando!"));
         mensajes.put("SALUDAR", () -> System.out.println("¡Saludos, viajero del tiempo y del código!"));
         mensajes.put("DESPEDIR", () -> System.out.println("Que los bits te acompañen, hasta la próxima misión."));
         mensajes.put("CANTAR", () -> System.out.println("01010101"));
@@ -26,7 +26,7 @@ public class MaquinaMisteriosa{
             case "DANZAR":
                 mensajes.get(comando).run();
             default:
-                System.out.printl("Comando no encontrado");
+                System.out.println("Comando no encontrado");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Solución Reto 6
 
+En este reto se creó una rama principal feature/reto_numero_6,
+y ramas para cada estudiante feature/reto6_jacobo y feature/reto6_MariaPaula.
+
 ### Uso de Map<String, Runnable>
 - Es muy interesante la forma que podemos simplificar operaciones usando un Map<String, Runable>.
 Lo que más nos soprendió es cómo nos ahorramos código dentro del switch.
@@ -8,4 +11,20 @@ Lo que más nos soprendió es cómo nos ahorramos código dentro del switch.
 
 ![Captura de pantalla 2025-09-04 213756.png](Imagenes/Captura%20de%20pantalla%202025-09-04%20213756.png)
 
->>>>>>> feature/reto6_jacobo
+- Código del compañero B:
+
+![Imagen1 (1).png](Imagenes/Imagen1%20%281%29.png)
+- Verificación de las ramas creadas:
+
+![Captura de pantalla 2025-09-04 220452.png](Imagenes/Captura%20de%20pantalla%202025-09-04%20220452.png)
+
+- Conflicto:
+
+![Captura de pantalla 2025-09-04 223541.png](Imagenes/Captura%20de%20pantalla%202025-09-04%20223541.png)
+
+![Captura de pantalla 2025-09-04 223554.png](Imagenes/Captura%20de%20pantalla%202025-09-04%20223554.png)
+
+
+- Solución del conflicto manualmente:
+
+![Captura de pantalla 2025-09-04 224133.png](Imagenes/Captura%20de%20pantalla%202025-09-04%20224133.png)
