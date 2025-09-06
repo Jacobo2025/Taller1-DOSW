272c97b0d211ba14acfbfeec0617d98112d64e8b
Jacobo <maria.rmunoz@mail.escuelaing.edu.co>
2025-09-05 19:40:48 -0500

    Carril 2: función para calcular el minimo con lambda

--- CarreraParalela.java
+++ CarreraParalela.java
@@ -1,5 +1,16 @@
+import java.util.*;
+import java.util.stream.*;
+
 public class CarreraParalela {
+    public static int encontrarMaximo(List<Integer> numeros) {
+        return numeros.stream()
+                      .max(Integer::compareTo)
+                      .orElseThrow(() -> new IllegalArgumentException("Lista vacía"));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Carrera paralela iniciada");
+        List<Integer> numeros = Arrays.asList(3, 8, 1, 6, 10, 2);
+        System.out.println("Máximo: " + encontrarMaximo(numeros));
     }
 }
+
