026c10eafa79ad3dba35fd67220f0fb93eda2ac3
Jacobo <conductordesitp@gmail.com>
2025-09-05 20:26:13 -0500

    Carril 2: función para obtener mínimo y cantidad de datos usando lambda

--- CarreraParalela.java
+++ CarreraParalela.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class CarreraParalela {
+    public static Map<String, Integer> minimoYCantidad(List<Integer> numeros) {
+
+        int minimo = numeros.stream()
+                            .min(Integer::compareTo)
+                            .get();
+
+        long cantidad = numeros.stream()
+                               .count();
+
+        Map<String, Integer> resultado = new HashMap<>();
+        resultado.put("minimo", minimo);
+        resultado.put("cantidad", (int) cantidad);
+        return resultado;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Carrera paralela iniciada");
+        List<Integer> numeros = Arrays.asList(4, 2, 7, 2, 5);
+
+        Map<String, Integer> res = minimoYCantidad(numeros);
+        System.out.println(res); 
     }
 }
+
+
