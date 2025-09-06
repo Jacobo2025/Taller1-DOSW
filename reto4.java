aedaf1ff5fb4eb2df3b1499ee0065b9f75939164
Maria Paula <maria.rmunoz@mail.escuelaing.edu.co>
2025-09-05 22:48:04 -0500

    Metodo combinar: Maria Paula

--- reto4.java
+++ reto4.java
@@ -5,3 +5,10 @@ public static Map<String, Integer> crearHashMap(List<Map.Entry<String, Integer>>
     }
     return mapa;
 }
+
+public static Map<String, Integer> combinarMapas(Map<String, Integer> hashMap, Map<String, Integer> hashtable) {
+    Map<String, Integer> resultado = new HashMap<>(hashMap);
+    hashtable.forEach((clave, valor) -> resultado.put(clave, valor));
+    return resultado;
+}
+
